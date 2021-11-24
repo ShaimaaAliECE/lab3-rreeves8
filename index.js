1fa19ef3ffa919b52d7cf25871d76724aeb34472
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2021-11-23 19:49:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const newConnection = require('./DBConnection')
 var bodyParser = require('body-parser')
 const { urlencoded } = require('body-parser')
 
-app.use(express.static('client/build', ))
+app.use(express.static(path.join(__dirname,'..client/build')))
 
 var jsonParser = bodyParser.json()
  
